299e981a9009d2b80a3a2ae3765f6851fe5722ee
krish2104 <75627540+krish2104@users.noreply.github.com>
2021-03-19 21:03:24 +0530

    Add files via upload

---
+++ paperball.js
@@ -0,0 +1,32 @@
+class paperball {
+    constructor(x,y,r){
+var options={
+     
+    restitution:0.3,
+    friction:0,
+    density:0.2,
+    isStatic:false
+    
+
+}
+this.x=x
+this.y=y
+this.r=r
+this.image=loadImage("paper.png")
+this.body=Bodies.circle(this.x,this.y,this.r,options)
+World.add(world,this.body)
+
+
+
+    }
+    display(){
+        var pos=this.body.position;
+        push()
+        translate(pos.x,pos.y)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r)
+        pop()
+
+    }
+
+}
